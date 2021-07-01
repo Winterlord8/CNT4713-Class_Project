5ebe1b4190bb66a970d9de4c3bc57a1e09be23d3
Jose Hernandez <josrhernandez1985@gmail.com>
2021-06-30 22:24:30 -0400

    proper deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #   import and setting the environment for local testing
-import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+# import os
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 from flask import Flask, redirect, url_for, render_template, Response
 from flask_dance.contrib.google import make_google_blueprint, google
@@ -9,11 +9,14 @@ from flask_dance.contrib.google import make_google_blueprint, google
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
 # setting up the google blueprint & registring it
 blueprint = make_google_blueprint(
                             client_id='56700923608-419gana0ifh33r7od61sj5daa5b9b7es.apps.googleusercontent.com',
                             client_secret='FBY5R9NwU_3BXrX4yQ3oNH4F',
-                            # reprompt_consent=True,
+                            reprompt_consent=True,
                             # offline=True,
                             scope=['profile', 'email']
                             )
