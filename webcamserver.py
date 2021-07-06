5781bc6f7295c55dc4674b3f0ca60966da2d3c62
Jose Hernandez <josrhernandez1985@gmail.com>
2021-07-06 18:02:31 -0400

    added cv2.CAP_DSHOW as a solution to a warning with openCV

--- webcamserver.py
+++ webcamserver.py
@@ -3,7 +3,7 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 
 def gen_frames():
@@ -21,9 +21,6 @@ def gen_frames():
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-
 
 
 if __name__ == '__main__':
